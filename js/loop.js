58f5ea39afff8f9ceb0232e7826a69120f1b2d82
ATHULBABS <athulbabuunni18@gmail.com>
2024-10-10 11:27:52 +0530

    sd

---
+++ js/loop.js
@@ -0,0 +1,31 @@
+
+// while loop
+
+// i=1
+// while(i<=10 )
+// {
+//     console.log(i)
+//     i++;
+// }
+
+// dowhile
+
+// i=1
+// do 
+// {
+//     console.log(i)
+//     i++;
+// }while(i<=5)
+
+
+// for loop
+
+// for(i=1;i<=8;i++)
+// {
+//     console.log(i)
+// }
+
+ for(i=0;i<=10;i+=2)
+ {
+    console.log(i)
+ }
